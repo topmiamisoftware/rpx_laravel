b1797d4bd7ae419a6ec318a40b19bebfb3d3aee1
Franco Petitfour <franco.petitfour001@gmail.com>
2020-11-07 02:12:48 -0500

    spotbie config

--- config/spotbie.php
+++ config/spotbie.php
@@ -2,7 +2,7 @@
 
     return [
 
-        'default_images_path' => env('DEFAULT_IMAGES_PATH', 'https://www.spotbie.com/'),
-        'background_images_path' => env('BACKGROUND_IMAGES_PATH', 'https://www.spotbie.com/')
+        'default_images_path' => env('DEFAULT_IMAGES_PATH', 'https://api.spotbie.com/defaults/'),
+        'background_images_path' => env('BACKGROUND_IMAGES_PATH', 'https://api.spotbie.com/backgrounds/')
 
     ];
