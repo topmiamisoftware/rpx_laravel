c6118d3538e6cd136521d98877e30d8cd1aa9fba
Franco Petitfour <franco.petitfour001@gmail.com>
2021-10-29 18:55:26 -0400

    Oct 29

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call(UserSeeder::class);
-        //$this->call(BusinessSeeder::class);
+        $this->call(BusinessSeeder::class);
     }
 }
