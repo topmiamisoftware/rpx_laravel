810e3b5e070c5fa8a041d13b1b04a5295deeae12
Franco Petitfour <franco.petitfour001@gmail.com>
2021-10-29 18:53:50 -0400

    oct 29

--- database/seeds/BusinessSeeder.php
+++ database/seeds/BusinessSeeder.php
@@ -3,6 +3,9 @@
 use App\Models\Business;
 use Illuminate\Database\Seeder;
 
+// composer require laracasts/testdummy
+use Laracasts\TestDummy\Factory as TestDummy;
+
 class BusinessSeeder extends Seeder
 {
     public function run()
