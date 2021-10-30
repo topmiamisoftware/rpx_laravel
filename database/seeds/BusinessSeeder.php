23d1a44db2d27bfa8a09908549b789032a26994f
Franco Petitfour <franco.petitfour001@gmail.com>
2021-10-30 08:24:22 -0400

    Oct 30

--- database/seeds/BusinessSeeder.php
+++ database/seeds/BusinessSeeder.php
@@ -15,6 +15,8 @@ class BusinessSeeder extends Seeder
     public function run()
     {
         $createBusiness = Business::factory()
+        ->hasRewards(5)
+        ->hasAds(4)
         ->count(10)
         ->create(); 
     }
