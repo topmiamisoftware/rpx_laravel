494d92d62304a1eeafbe5feaf99b275bdda7c101
Franco Petitfour <franco.petitfour001@gmail.com>
2021-10-31 16:02:01 -0400

    Oct 31

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -59,6 +59,50 @@ class UserSeeder extends Seeder
         ->hasLoyaltyPointBalance(1)
         ->create();  
 
+        /* Account #1 */
+        User::factory(
+            function (array $attributes){
+                return [
+                    'email' => 'agent000@spotbie.com',
+                    'username' => 'agent000',
+                    'password' => 'HelloWorld33!'
+                ];                        
+            }            
+        )
+        ->count(10)
+        ->hasSpotbieUser(1, function (array $attributes){
+            return [
+                'first_name' => 'Franco',
+                'last_name' => 'Petitfour',
+                'user_type' => 4
+            ];                        
+        })
+        ->hasBusiness(1)
+        ->hasLoyaltyPointBalance(1)
+        ->create();  
+
+        /* Account #2 */
+        User::factory(
+            function (array $attributes){
+                return [
+                    'email' => 'agent001@spotbie.com',
+                    'username' => 'agent001',
+                    'password' => 'HelloWorld33!'
+                ];                        
+            }
+        )
+        ->count(10)
+        ->hasSpotbieUser(1, function (array $attributes){
+            return [
+                'first_name' => 'Andres',
+                'last_name' => 'Garcia',
+                'user_type' => 4
+            ];                        
+        })
+        ->hasBusiness(1)
+        ->hasLoyaltyPointBalance(1)
+        ->create();  
+
     }
     
 }
