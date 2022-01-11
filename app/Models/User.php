032bd82da4f1471d9575fa40dea08066178721cf
Franco Petitfour <franco.petitfour001@gmail.com>
2022-01-10 21:32:55 -0500

    userbullable

--- app/Models/User.php
+++ app/Models/User.php
@@ -1655,7 +1655,7 @@ class User extends Authenticatable implements JWTSubject
 
         $userBillable = Cashier::findBillable($user->stripe_id);
         
-        if($userBillable !== null)
+        if(!is_null($userBillable))
             $userBillable->subscription($user->id)->cancelNow();
         
         //We also need to cancel all of the user's ads if they have any.
