25a381446bc47efd88d61b42231af5eacecb1a97
0456franco <franco.petitfour001@gmail.com>
2022-12-19 21:59:36 -0500

    Set Up Logging for Stripe Webhook Event Listener

--- app/Listeners/StripeEventListener.php
+++ app/Listeners/StripeEventListener.php
@@ -22,27 +22,21 @@ class StripeEventListener
     public function handle(WebhookReceived $event)
     {
 
-        
+
         /**
-         *  
-         * We have two types of customers. 
+         *
+         * We have two types of customers.
          * 1. First type of stripe customer is the Ads models which each Ad has a different subscription.
          * 2. Second type of stripe customer is the Users models which each Users has a different subscription.
          *
          */
 
         if ($event->payload['type'] === 'customer.subscription.updated') {
-            
-
-
+            Log::info('Customer Subscription Updated:', ['info' => $event->payload]);
         } else if($event->payload['type'] === 'customer.created') {
-
-
-
+            Log::info('Customer Subscription Updated:', ['info' => $event->payload]);
         } else if($event->payload['type'] === 'customer.subscription.deleted') {
-            
-
-
+            Log::info('Customer Subscription Updated:', ['info' => $event->payload]);
         }
     }
 }
