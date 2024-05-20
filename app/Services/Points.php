228d639d971d84cb1294a019aab4622377735cfb
0456franco <franco.petitfour001@gmail.com>
2024-05-20 11:13:14 -0400

    ADDED: SendRewardRedeemedSms.php and SendPointsRedeemedSms.php to SMS users when they redeem rewards or points.

---
+++ app/Services/Points.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Services;
+
+use App\Helpers\Sms\SmsAndCallTwimlHelper;
+use App\Models\Sms;
+use App\Models\SmsGroup;
+use Twilio\Exceptions\TwilioException;
+use Twilio\Rest\Client;
+use Illuminate\Support\Facades\Log;
+
+class Points
+{
+    public function redeemedPoints(
+        string $userPhoneNumber,
+        string $userID,
+        string $rewardName,
+        string $businessName,
+        Sms $sms,
+        SmsGroup $smsGroup,
+        bool $withLoginInstructions
+    ) {
+        try
+        {
+            $lang = 'en';
+            $sid = config('services.twilio.account_sid');
+            $token = config('services.twilio.token');
+
+            $client = new Client($sid, $token);
+            $langHelper = new SmsAndCallTwimlHelper($lang);
+            $body = $langHelper->getPointsRedeemedSmsTxt($rewardName, $businessName, $withLoginInstructions);
+
+            $client->messages->create(
+                $userPhoneNumber,
+                [
+                    'from' => config('services.twilio.from'),
+                    'body' => $body,
+                ]
+            );
+
+            // Update SMS message in DB;
+            $sms->update([
+                'sent' => true,
+            ]);
+
+            $smsGroup->update([
+                'total_sent' => $smsGroup->total_sent + 1,
+                'price' => $smsGroup->price + 0.0079
+            ]);
+
+            Log::info(
+                '[CustomerManager]-[sendSms]: Message Sent' .
+                ', User ID: '. $userID .
+                ', Phone-Number: ' . $userPhoneNumber .
+                ', Business: ' . $businessName .
+                ', Reward Name: ' . $rewardName
+            );
+        }
+        catch(TwilioException $e)
+        {
+            $errorCode = '';
+            switch($e->getCode())
+            {
+                case '21211':
+                    $errorCode = 'phoneNumber.invalid';
+                    break;
+                case '21612':
+                case '21408':
+                case '21610':
+                case '21614':
+                    $errorCode = 'phoneNumber.unavailable';
+                    break;
+                default:
+                    $errorCode = $e->getCode();
+            }
+
+            Log::error(
+                '[CustomerManager]-[sendSms]: Message Failed' .
+                ', Phone-Number: ' . $userPhoneNumber .
+                ", Business: " . $businessName .
+                ", Error Code: " . $errorCode .
+                ", Error Message: " . $e->getMessage()
+            );
+
+            return $errorCode;
+        }
+    }
+}
