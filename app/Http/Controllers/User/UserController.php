d717acbddb0b1fa1e881b3c8ec1ed515b1678b4b
0456franco <franco.petitfour001@gmail.com>
2024-07-20 22:52:46 -0400

    ADDED: temp. validator for phone numbers saved through the user app

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
 
     public function update(Request $request, User $user)
     {
-        if (array_key_exists('phone_number', $request) && !str_contains($request->phone_number, '+1')) {
+        if (isset($request->phone_number) && ! str_contains($request->phone_number, '+1')) {
             $request->phone_number = '+1'.$request->phone_number;
         }
 
