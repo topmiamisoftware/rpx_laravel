88b5f21d4f3c8257f3198ddf39505e45aced52e8
0456franco <franco.petitfour001@gmail.com>
2024-09-02 22:53:37 -0400

    CHANGED: fix issue with User refresh call

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,7 +69,7 @@ class User extends Authenticatable implements JWTSubject
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function refresh()
+    public function refreshWToken()
     {
         return $this->respondWithToken(Auth::refresh());
     }
@@ -188,7 +188,7 @@ class User extends Authenticatable implements JWTSubject
 
         $newSpotbieUser = $user->spotbieUser()->select('default_picture', 'user_type')->first();
 
-        //Start the session
+        // Start the session
         Auth::login($user);
 
         if (config('env') === 'production') {
@@ -1045,7 +1045,7 @@ class User extends Authenticatable implements JWTSubject
                 $loyaltyPointBalance->save();
             }, 3);
 
-            $user = $user->refresh();
+            $user = Cashier::findBillable($userStripeId);
         }
 
         $response = [
