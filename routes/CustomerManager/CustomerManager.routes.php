0600f7adfedb559277ab1203e80d6c254954359f
0456franco <franco.petitfour001@gmail.com>
2024-11-11 15:58:49 -0500

    CustomerManager

--- routes/CustomerManager/CustomerManager.routes.php
+++ routes/CustomerManager/CustomerManager.routes.php
@@ -7,5 +7,5 @@ Route::get('index', [CustomerManager::class, 'index'])->middleware('auth');
 Route::post('sms', [CustomerManager::class, 'sms'])->middleware('auth');
 Route::get('sms-group-list', [CustomerManager::class, 'smsGroupList'])->middleware('auth');
 Route::post('email', [CustomerManager::class, 'email'])->middleware('auth');
-Route::post('send-promotion', [CustomerManager::class, 'sendPromotion`'])->middleware('auth');
+Route::post('send-promotion', [CustomerManager::class, 'sendPromotion'])->middleware('auth');
 Route::get('email-group-list', [CustomerManager::class, 'emailGroupList'])->middleware('auth');
