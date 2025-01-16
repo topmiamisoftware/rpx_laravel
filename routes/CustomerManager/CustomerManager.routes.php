4f395324205bb3d9147116fb822a971d9b07fb2d
0456franco <franco.petitfour001@gmail.com>
2025-01-16 00:32:22 -0500

    REMOVED ROUTES FOR SMS

--- routes/CustomerManager/CustomerManager.routes.php
+++ routes/CustomerManager/CustomerManager.routes.php
@@ -7,6 +7,6 @@ Route::get('index', [CustomerManager::class, 'index'])->middleware('auth');
 Route::post('sms', [CustomerManager::class, 'sms'])->middleware('auth');
 Route::get('sms-group-list', [CustomerManager::class, 'smsGroupList'])->middleware('auth');
 Route::post('email', [CustomerManager::class, 'email'])->middleware('auth');
-Route::post('send-promotion', [CustomerManager::class, 'sendPromotion'])->middleware('auth');
+// Route::post('send-promotion', [CustomerManager::class, 'sendPromotion'])->middleware('auth');
 Route::get('email-group-list', [CustomerManager::class, 'emailGroupList'])->middleware('auth');
 Route::get('get-promotion', [CustomerManager::class, 'getPromotion'])->middleware('auth');
